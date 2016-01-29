de860d62d14be3c3405897ea6cfac9af11c529f2
Lorenz Schmoliner <lorenz.schmoliner@gmail.com>
2016-01-29 20:50:33 +0100

    added initial project

---
+++ desktop/src/at/ggjg/evg/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package at.ggjg.evg.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import at.ggjg.evg.EvilBunnyGodGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new EvilBunnyGodGame(), config);
+	}
+}
