665d4590ec76dca1ed2de4a0c7c16f2c3c8ffa9d
Xifiggam <xifiggam@gmx.at>
2016-01-29 23:08:36 +0100

    Bugfix

--- core/src/at/ggjg/evg/entities/GameObject.java
+++ core/src/at/ggjg/evg/entities/GameObject.java
@@ -2,7 +2,6 @@ package at.ggjg.evg.entities;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
-import com.github.dreamsnatcher.utils.Assets;
 
 public abstract class GameObject {
     public Vector2 position;
