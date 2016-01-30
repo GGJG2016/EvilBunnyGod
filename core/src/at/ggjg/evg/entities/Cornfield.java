f47bea9f999090af4fe1c7303f2b5da7dee0a629
Christopher <cgalle1@Gmail.com>
2016-01-30 14:16:11 +0100

    added cornfield initial collision detection

---
+++ core/src/at/ggjg/evg/entities/Cornfield.java
@@ -0,0 +1,20 @@
+package at.ggjg.evg.entities;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import at.ggjg.evg.mechanic.World;
+
+/**
+ * Created by christophergalle on 30/01/16.
+ */
+public class Cornfield extends GameObject {
+    @Override
+    public void init(World world) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+
+    }
+}
