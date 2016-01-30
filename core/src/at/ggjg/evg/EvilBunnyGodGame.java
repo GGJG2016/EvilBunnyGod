c9587bbc37dbb868fcda501648c4171e6cf55987
Christian Zellot <Christian Zellot>
2016-01-30 10:51:00 +0100

    merge conflict fix

--- core/src/at/ggjg/evg/EvilBunnyGodGame.java
+++ core/src/at/ggjg/evg/EvilBunnyGodGame.java
@@ -5,7 +5,12 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.input.GestureDetector;
 
+import java.util.ArrayList;
+
+import at.ggjg.evg.gestures.Sequence;
+import at.ggjg.evg.gestures.SequenceHolder;
 import at.ggjg.evg.screens.GameplayScreen;
 import at.ggjg.evg.screens.Screen;
 import at.ggjg.evg.screens.ScreenManager;
@@ -19,12 +24,25 @@ public class EvilBunnyGodGame extends ApplicationAdapter {
 		manager = new ScreenManager();
 		Screen screen = new GameplayScreen(manager,1);
 		manager.setScreen(screen);
+//		ArrayList<Sequence> sequenceList = new ArrayList<Sequence>();
+//		//define sequences .. item which first matches wins
+//		sequenceList.add(new Sequence(Sequence.SequenceName.CIRCLE, 0, 3, 6, 9, 10, 11, 8, 5, 2, 1));
+//		sequenceList.add(new Sequence(Sequence.SequenceName.LINE, 0, 1, 2));
+//		SequenceHolder sequenceHolder = new SequenceHolder(24, sequenceList);
+//		Gdx.input.setInputProcessor(new GestureDetector(new at.ggjg.evg.gestures.SequenceGestureListener(sequenceHolder, Gdx.graphics.getHeight(), Gdx.graphics.getWidth())));
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+
+//		Sequence match = sequenceHolder.getMatch();
+//		if(match != null){
+//			System.out.println("sequence was a circle " + match.getSequenceName());
+//			sequenceHolder.clearLastArea();
+//		}
 		manager.render();
 	}
 
