9dedda3da4db0006dd051fff250dfa51d0a9a4b2
Lorenz Schmoliner <lorenz.schmoliner@gmail.com>
2016-01-30 15:13:31 +0100

    map click listener added, OnMapClickedListener interface

---
+++ core/src/at/ggjg/evg/helpers/OnMapClickedListener.java
@@ -0,0 +1,9 @@
+package at.ggjg.evg.helpers;
+
+/**
+ * Created by jarhoax on 1/30/16.
+ */
+public interface OnMapClickedListener {
+
+    void onMapClicked(float x, float y, boolean isRightClick);
+}
--- core/src/at/ggjg/evg/screens/GameplayScreen.java
+++ core/src/at/ggjg/evg/screens/GameplayScreen.java
@@ -1,15 +1,15 @@
 package at.ggjg.evg.screens;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.input.GestureDetector;
-
-import java.util.ArrayList;
-
 import at.ggjg.evg.AudioManager;
 import at.ggjg.evg.gestures.Sequence;
 import at.ggjg.evg.gestures.SequenceHolder;
 import at.ggjg.evg.mechanic.World;
 import at.ggjg.evg.mechanic.WorldRenderer;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+import java.util.ArrayList;
 
 /**
  * Created by Veit on 29.01.2016.
@@ -21,15 +21,18 @@ public class GameplayScreen extends Screen {
     int lvl;
     SequenceHolder sequenceHolder;
 
-    public GameplayScreen (ScreenManager manager, int lvl) {
+    public GameplayScreen(ScreenManager manager, int lvl) {
         super(manager);
         this.lvl = lvl;
-        switch(lvl){
-            case 0: world = new World("levels/testMap.tmx");
+        switch (lvl) {
+            case 0:
+                world = new World("levels/testMap.tmx");
                 break;
-            case 1: world = new World("levels/map1_v2.tmx");
+            case 1:
+                world = new World("levels/map1_v2.tmx");
                 break;
-            case 2: world = new World("levels/map2_v1.tmx");
+            case 2:
+                world = new World("levels/map2_v1.tmx");
                 break;
         }
         renderer = new WorldRenderer(world);
@@ -37,25 +40,25 @@ public class GameplayScreen extends Screen {
         world.setRenderer(renderer);
         world.setAudio(audio);
         ArrayList<Sequence> sequenceList = new ArrayList<Sequence>();
-		//define sequences .. item which first matches wins
-		sequenceList.add(new Sequence(Sequence.SequenceName.CIRCLE, 0, 3, 6, 9, 10, 11, 8, 5, 2, 1));
-		sequenceList.add(new Sequence(Sequence.SequenceName.LINE, 0, 1, 2));
-		sequenceHolder = new SequenceHolder(24, sequenceList);
-		Gdx.input.setInputProcessor(new GestureDetector(new at.ggjg.evg.gestures.SequenceGestureListener(sequenceHolder, Gdx.graphics.getHeight(), Gdx.graphics.getWidth())));
+        //define sequences .. item which first matches wins
+        sequenceList.add(new Sequence(Sequence.SequenceName.CIRCLE, 0, 3, 6, 9, 10, 11, 8, 5, 2, 1));
+        sequenceList.add(new Sequence(Sequence.SequenceName.LINE, 0, 1, 2));
+        sequenceHolder = new SequenceHolder(24, sequenceList);
+        Gdx.input.setInputProcessor(new GestureDetector(new at.ggjg.evg.gestures.SequenceGestureListener(world, sequenceHolder, Gdx.graphics.getHeight(), Gdx.graphics.getWidth())));
     }
 
     @Override
-    public void render () {
-       float delta = Gdx.graphics.getDeltaTime();
+    public void render() {
+        float delta = Gdx.graphics.getDeltaTime();
 //
 //        if(delta > 1.0f)
 //            delta = 0.0f;
 //
         Sequence match = sequenceHolder.getMatch();
-		if(match != null){
-			System.out.println("sequence was a " + match.getSequenceName());
-			sequenceHolder.clearLastArea();
-		}
+        if (match != null) {
+            System.out.println("sequence was a " + match.getSequenceName());
+            sequenceHolder.clearLastArea();
+        }
         renderer.render(delta);
         audio.update(delta);
         world.update(delta);
@@ -78,7 +81,12 @@ public class GameplayScreen extends Screen {
     }
 
     @Override
-    public void dispose () {
+    public void resize(int width, int height) {
+        renderer.resize(width, height);
+    }
+
+    @Override
+    public void dispose() {
 //        renderer.dispose();
 //        audio.dispose();
 //        world.dispose();
