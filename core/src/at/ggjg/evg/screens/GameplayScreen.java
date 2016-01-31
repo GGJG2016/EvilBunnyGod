fa6e71c41b0989f58f5c4d4a518a50f2520dbf00
badlogic <badlogicgames@gmail.com>
2016-01-31 13:42:24 +0100

    Removed debug rendering

--- core/src/at/ggjg/evg/screens/GameplayScreen.java
+++ core/src/at/ggjg/evg/screens/GameplayScreen.java
@@ -25,9 +25,6 @@ public class GameplayScreen extends Screen {
     AudioManager audio;
     int lvl;
     SequenceHolder sequenceHolder;
-    OrthographicCamera uiCam;
-    SpriteBatch batch;
-    BitmapFont font;
 
     public GameplayScreen(ScreenManager manager, int lvl) {
         super(manager);
@@ -56,11 +53,6 @@ public class GameplayScreen extends Screen {
         world.setManager(manager);
         initGestures();
         Gdx.input.setInputProcessor(new GestureDetector(new at.ggjg.evg.gestures.SequenceGestureListener(world, sequenceHolder, Gdx.graphics.getHeight(), Gdx.graphics.getWidth())));
-        
-        uiCam = new OrthographicCamera();
-        uiCam.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        batch = new SpriteBatch();
-        font = new BitmapFont();
     }
 
     private void initGestures() {
@@ -89,14 +81,6 @@ public class GameplayScreen extends Screen {
         renderer.render(delta);
         audio.update(delta);
         world.update(delta);
-        
-        uiCam.update();
-        batch.setProjectionMatrix(uiCam.combined);
-        batch.begin();
-        Vector3 pos = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
-        renderer.camera.unproject(pos);
-        font.draw(batch, pos.x + ", " + pos.y, 0, 10);
-        batch.end();
     }
 
     @Override
